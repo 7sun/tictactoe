47857ca7ed88b0a515a19262503663e2e9b305a7
7sun <pakrouse@gmail.com>
2014-12-15 17:43:25 -0800

    Adds scoreboard

--- js/ttt.js
+++ js/ttt.js
@@ -1,15 +1,21 @@
 var boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 var startingPlayer = "player one";
 var currentPlayer = "player one";
-// var squareZero = document.getElementById("square-zero"); 
+var scoreBoard = [0, 0, 0] //[Ties, Player One Wins, Player Two Wins]
+var ties = document.getElementById("ties");
+var playerOneWinsCounter = document.getElementById("player-one-wins");
+var playerTwoWinsCounter = document.getElementById("player-two-wins");
 
 
 function resetGame() {
-	var divs = document.getElementsByTagName("div")
+	var boardClass = document.getElementsByClassName("board-square")
 	for (i = 0; i < boardArray.length; i++) {
 		boardArray[i] = 0;
-		divs[i].style.backgroundColor = "#000";
+		boardClass[i].style.backgroundColor = "#000";
 	}
+	ties.innerHTML = scoreBoard[0].toString();
+	playerOneWinsCounter.innerHTML = scoreBoard[1].toString();
+	playerTwoWinsCounter.innerHTML = scoreBoard[2].toString();
 	changeStartingPlayer();
 };
 
@@ -59,41 +65,49 @@ function playSquare(squareNum, idName) {
 function outcome(playerNum){
 	if (boardArray[0] == playerNum && boardArray[0] == boardArray[1] && boardArray[1] == boardArray[2]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[3] == playerNum && boardArray[3] == boardArray[4] && boardArray[4] == boardArray[5]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[6] == playerNum && boardArray[6] == boardArray[7] && boardArray[7] == boardArray[8]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[0] == playerNum && boardArray[0] == boardArray[3] && boardArray[3] == boardArray[6]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[1] == playerNum && boardArray[1] == boardArray[4] && boardArray[4] == boardArray[7]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[2] == playerNum && boardArray[2] == boardArray[5] && boardArray[5] == boardArray[8]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[0] == playerNum && boardArray[0] == boardArray[4] && boardArray[4] == boardArray[8]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
 	else if (boardArray[2] == playerNum && boardArray[2] == boardArray[4] && boardArray[4] == boardArray[6]){
 		alert("Player " + playerNum + " wins!");
+		scoreBoard[playerNum] += 1;
 		resetGame();
 	}
 
@@ -106,6 +120,7 @@ function outcome(playerNum){
 function checkForTie(){
 	if (boardArray.indexOf(0) == -1) {
 		alert("Tie Game.");
+		scoreBoard[0] += 1;
 		resetGame();
 	}
 };
